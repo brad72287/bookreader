6d183deb869cc6315af87297b53bb61cae166b1f
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-08-19 12:35:32 -0400

    worked on render page using epub.js

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -61,6 +61,9 @@ class BooksController < ApplicationController
     end
   end
 
+  def epub
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
