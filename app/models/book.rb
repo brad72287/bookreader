214e1f2ee2ff6e72cb58dabdfb5acc89e4e84962
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-08-19 15:37:25 -0400

    installed devise gem

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,11 @@
 class Book < ApplicationRecord
 
 	has_many :bookgenres
-	has_many :books, through: :bookgenres
+	has_many :genres, through: :bookgenres
+
+	validates :title, presence: true
+	validates :author, presence: true
+
 	has_attached_file :epub
 	validates_attachment :epub, presence: true,
 	  content_type: { content_type: "application/epub+zip" }
