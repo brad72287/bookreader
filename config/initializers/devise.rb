446d4298b5f294a139182e60a4d8b28f2f46c773
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-08-19 16:24:35 -0400

    attempting to configure omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,7 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :facebook, "296028894078431", "82b93f5f2218a1c706eadda9299ba839", callback_url: "http://localhost:3000/"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
