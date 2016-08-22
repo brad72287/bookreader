b0e3be245e2c4eab7fb476b95b3bcebfc1195123
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-08-22 16:12:30 -0400

    created a table partial to DRY up the views

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 # export FACEBOOK_KEY=296028894078431
 # export FACEBOOK_SECRET=82b93f5f2218a1c706eadda9299ba839
 #
-
 # Rails.application.config.middleware.use OmniAuth::Builder do
 #       provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 # end
