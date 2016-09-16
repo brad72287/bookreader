82cce2ec163f96c978cbd1d4d44cf5bb0d5b9cf7
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-09-16 10:35:09 -0400

    moved Comment and Genre classes from application.js to new file classes.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,32 +17,5 @@
 //= require bootstrap-sprockets
 
 
-class Genre {
-  constructor(genreJSON) {
-    this.name = genreJSON.name;
-    this.id = genreJSON.id;
-    this.books = genreJSON.books;
-  }
- 
-  formatGenre() {
-    return "<h4><a href='/genres/"+this.id+"'>"+this.name+" <span class='badge'>"+this.books.length+"</span></a></td></h4>";
-  }
-}
-
-class Comment {
-  constructor(commentJSON) {
-    this.content = commentJSON.content;
-    this.id = commentJSON.id;
-    this.user_id = commentJSON.user_id;
-    this.book_id = commentJSON.book_id;
-    this.user = commentJSON.user;
-  }
- 
-  formatComment() {
-    return "<tr><td><strong>"+this.user.email+"</strong> says: "+this.content+"</td><td><a data-confirm='Are you sure?'' rel='nofollow' data-method='delete' href='/comments/"+self.id+"'>delete</a></td></tr>";
-  }
-}
-
-
 
 
